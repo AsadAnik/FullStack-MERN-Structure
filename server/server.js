210ff2fb3c701860046175717bfaeb1f8a6b972a
AsadAnik <s.engineer63@gmail.com>
2022-10-17 13:58:41 +0600

    Complete Setup of MERN Stack v0.2

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
-const express = require('./config/express.js'),
-	app = express.init();
+const http = require('http'),
+    app = require('./app/app'),
+    config = require('./config/config').get(process.env.NODE_ENV),
+    connectDB = require('./db/db');
 
-// Use ENV PORT or default..
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "localhost";
+const HOST = config.HOST;
+const PORT = config.PORT;
+const DB_URI = config.DB_URI || process.env.MONGO_URI;
 
-// listening server..
-app.listen(PORT, () => {
-	console.log(`Server is now running on http://${HOST}:${PORT}`);
-});
+// Node Server..
+const server = http.createServer(app);
+
+// DB Connection and server Listeninig..
+connectDB(DB_URI)
+    .then(() => {
+        console.log('--------- Database is connected! -------');
+        // Listening to Server..
+        server.listen(PORT, HOST, () => {
+            console.log(`Welcome to -- ${process.env.APP_NAME} -- `);
+            console.log(`Server is running on http://${HOST}:${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.log('ERR! Can\'t Connected with Database! -->', error.message);
+    });
